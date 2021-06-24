e06f5ff1b4df4deb07837a48178f575a7066bd87
s7rul <eriser-8@student.ltu.se>
2021-06-24 12:27:53 +0200

    slightly changed structure

--- src/lookupTable/LookupTable.java
+++ src/lookupTable/LookupTable.java
@@ -5,17 +5,20 @@ import java.util.Hashtable;
 public class LookupTable {
 	private Hashtable<String, DeviceType> table = new Hashtable<>();
 	
-	public void add(String name) {
-		if (!table.containsKey(name)) {
-			DeviceType newDeviceType = new DeviceType(name, DeviceType.Compatabilety.YES);
-			this.table.put(name, newDeviceType);
+	public void add(DeviceType input) {
+		if (!table.containsKey(input.getName())) {
+			this.table.put(input.getName(), input);
 		} else {
-			// Device allready in table
-			// TODO: Add error (learn java erroros)
+			// Device already in table
+			// TODO: Add error (learn java errors)
 		}
 	}
 	
-	public DeviceType getDeviceTypeFromName(String name) {
+	public void remove(String name) {
+		this.table.remove(name);
+	}
+	
+	public DeviceType getDeviceTypeByName(String name) {
 		return table.get(name);
 	}
 	
@@ -23,11 +26,21 @@ public class LookupTable {
 		// fast dirty test
 		LookupTable tt = new LookupTable();
 		
-		tt.add("test1");
-		tt.add("test3");
+		DeviceType t1 = new DeviceType("test1", DeviceType.Compatabilety.YES);
+		DeviceType t2 = new DeviceType("test2", DeviceType.Compatabilety.NO);
+		DeviceType t3 = new DeviceType("test2", DeviceType.Compatabilety.YES);
+		DeviceType t4 = new DeviceType("test3", DeviceType.Compatabilety.WITH_REQUIREMENT);
+		Requirement r1 = new Requirement("ram", Requirement.TypeEnum.EQUAL_OR_MORE, 512);
+		t4.addRequirement(r1);
 		
-		System.out.println("Test1 " + tt.getDeviceTypeFromName("test1"));
-		System.out.println("Test2 " + tt.getDeviceTypeFromName("test2"));
+		tt.add(t1);
+		tt.add(t2);
+		tt.add(t3);
+		tt.add(t4);
+		
+		System.out.println("Test1 " + tt.getDeviceTypeByName("test1"));
+		System.out.println("Test2 " + tt.getDeviceTypeByName("test2"));
+		System.out.println("Test3 " + tt.getDeviceTypeByName("test3"));
 	}
 		
 }
