4aaecace09fed82f93c4fab0e740ed25977aefc3
s7rul <eriser-8@student.ltu.se>
2021-06-28 09:04:22 +0200

    main algoritmh function finished in first state

--- src/lookupTable/GenerationFeasibilityTester.java
+++ src/lookupTable/GenerationFeasibilityTester.java
@@ -1,6 +1,7 @@
 package lookupTable;
 
 import deviceRegistryDummy.DeviceRegistry;
+import deviceRegistryDummy.Device;
 
 public class GenerationFeasibilityTester {
 
@@ -13,10 +14,23 @@ public class GenerationFeasibilityTester {
 
         // Only for testing remove when integrating with the real device registry
         this.registry.populate();
+        this.table.populate();
     }
 
-    public Boolean GenerationFeasibilityByDeviceID(Integer deviceID) {
-        Boolean ret = false;
-        return ret;
+    public Boolean generationFeasibilityByDeviceID(Integer deviceID) {
+        // TODO: add proper error handling
+        Device d = registry.getDeviceByID(deviceID);
+        if (d == null) {
+            System.out.println("device not in device registry");
+            return false;
+        }
+
+        DeviceType type = table.getDeviceTypeByName(d.getDeviceType());
+        if (type == null) {
+            System.out.println("device type not in table");
+            return false;
+        } else {
+            return type.validate(d.getMetaData());
+        }
     }
 }
