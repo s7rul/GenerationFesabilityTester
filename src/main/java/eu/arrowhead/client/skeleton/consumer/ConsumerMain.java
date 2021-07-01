1cd0836b51c3a8fcd58c4a4670c00bddd37eb723
s7rul <eriser-8@student.ltu.se>
2021-07-01 09:17:05 +0200

    Started making it into a arrowhead consumer

---
+++ src/main/java/eu/arrowhead/client/skeleton/consumer/ConsumerMain.java
@@ -0,0 +1,118 @@
+package eu.arrowhead.client.skeleton.consumer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpMethod;
+
+import eu.arrowhead.client.library.ArrowheadService;
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.SSLProperties;
+import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.dto.shared.OrchestrationFlags.Flag;
+import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO;
+import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO.Builder;
+import eu.arrowhead.common.dto.shared.OrchestrationResponseDTO;
+import eu.arrowhead.common.dto.shared.OrchestrationResultDTO;
+import eu.arrowhead.common.dto.shared.ServiceInterfaceResponseDTO;
+import eu.arrowhead.common.dto.shared.ServiceQueryFormDTO;
+import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.common.exception.InvalidParameterException;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {CommonConstants.BASE_PACKAGE}) //TODO: add custom packages if any
+public class ConsumerMain implements ApplicationRunner {
+    
+    //=================================================================================================
+	// members
+	
+    @Autowired
+	private ArrowheadService arrowheadService;
+    
+    @Autowired
+	protected SSLProperties sslProperties;
+    
+	private final Logger logger = LogManager.getLogger( ConsumerMain.class );
+    
+    //=================================================================================================
+	// methods
+
+	//------------------------------------------------------------------------------------------------
+    public static void main( final String[] args ) {
+    	SpringApplication.run(ConsumerMain.class, args);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    @Override
+	public void run(final ApplicationArguments args) throws Exception {
+    	logger.info("Orchestration request for test service:");
+    	final ServiceQueryFormDTO serviceQueryForm = new ServiceQueryFormDTO.Builder(ConsumerConstants.TEST_SERVICE_DEFINITION)
+    																		.interfaces(getInterface())
+    																		.build();
+    	
+		final Builder orchestrationFormBuilder = arrowheadService.getOrchestrationFormBuilder();
+		final OrchestrationFormRequestDTO orchestrationFormRequest = orchestrationFormBuilder.requestedService(serviceQueryForm)
+																					   .flag(Flag.MATCHMAKING, true)
+																					   .flag(Flag.OVERRIDE_STORE, true)
+																					   .build();
+		
+		printOut(orchestrationFormRequest);		
+		
+		final OrchestrationResponseDTO orchestrationResponse = arrowheadService.proceedOrchestration(orchestrationFormRequest);
+		
+		logger.info("Orchestration response:");
+		printOut(orchestrationResponse);		
+		
+		if (orchestrationResponse == null) {
+			logger.info("No orchestration response received");
+		} else if (orchestrationResponse.getResponse().isEmpty()) {
+			logger.info("No provider found during the orchestration");
+		} else {
+			final OrchestrationResultDTO orchestrationResult = orchestrationResponse.getResponse().get(0);
+			validateOrchestrationResult(orchestrationResult, ConsumerConstants.TEST_SERVICE_DEFINITION);
+			
+			logger.info("Create a request:");
+			final HttpMethod httpMethod = HttpMethod.valueOf(orchestrationResult.getMetadata().get(ConsumerConstants.HTTP_METHOD));
+			final String token = orchestrationResult.getAuthorizationTokens() == null ? null : orchestrationResult.getAuthorizationTokens().get(getInterface());
+			/*final String sr = arrowheadService.consumeServiceHTTP(String.class, HttpMethod.valueOf(orchestrationResult.getMetadata().get(ConsumerConstants.HTTP_METHOD)),
+					orchestrationResult.getProvider().getAddress(), orchestrationResult.getProvider().getPort(), orchestrationResult.getServiceUri(),
+					getInterface(), token, null, new String[0]);*/
+			final String sr = arrowheadService.consumeServiceHTTP(String.class, httpMethod,
+					orchestrationResult.getProvider().getAddress(), orchestrationResult.getProvider().getPort(), orchestrationResult.getServiceUri(),
+					getInterface(), token, null, new String[0]);
+			logger.info("Provider response");
+			printOut(sr);
+				
+		}
+	}
+    
+    private void printOut(final Object object) {
+    	System.out.println(Utilities.toPrettyJson(Utilities.toJson(object)));
+    }
+    
+    private String getInterface() {
+    	return sslProperties.isSslEnabled() ? ConsumerConstants.INTERFACE_SECURE : ConsumerConstants.INTERFACE_INSECURE;
+    }
+    
+    private void validateOrchestrationResult(final OrchestrationResultDTO orchestrationResult, final String serviceDefinitin) {
+    	if (!orchestrationResult.getService().getServiceDefinition().equalsIgnoreCase(serviceDefinitin)) {
+			throw new InvalidParameterException("Requested and orchestrated service definition do not match");
+		}
+    	
+    	boolean hasValidInterface = false;
+    	for (final ServiceInterfaceResponseDTO serviceInterface : orchestrationResult.getInterfaces()) {
+			if (serviceInterface.getInterfaceName().equalsIgnoreCase(getInterface())) {
+				hasValidInterface = true;
+				break;
+			}
+		}
+    	if (!hasValidInterface) {
+    		throw new InvalidParameterException("Requested and orchestrated interface do not match");
+		}
+    }
+}
